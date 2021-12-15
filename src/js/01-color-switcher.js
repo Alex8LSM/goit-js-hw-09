68569bd0634a8ea13647bc5ef086ee99d58460cd
Oleksandr Khvaleba <alex8inbox@gmail.com>
2021-12-15 02:00:07 +0200

    Task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-const starnBtn = document.querySelector('button[data-start]');
+const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let myTimerId = null;
 stopBtn.style.background = 'grey';
 
 const startFunc = () => {
   myTimerId = setInterval(changeBackground, 1000);
-  starnBtn.setAttribute('disabled', 'disabled');
-  starnBtn.style.background = 'grey';
+  startBtn.setAttribute('disabled', 'disabled');
+  startBtn.style.background = 'grey';
   stopBtn.removeAttribute('disabled');
   stopBtn.style.removeProperty('background');  
 
 };
 const stopFunc = () => {
   clearInterval(myTimerId);
-    starnBtn.removeAttribute('disabled');
+    startBtn.removeAttribute('disabled');
     stopBtn.setAttribute('disabled', 'disabled');
-    starnBtn.style.removeProperty('background');
+    startBtn.style.removeProperty('background');
     stopBtn.style.background = 'grey';
 };
 
 function changeBackground() {
   document.body.style.background = getRandomHexColor();
 }
-starnBtn.addEventListener('click', startFunc);
+startBtn.addEventListener('click', startFunc);
 stopBtn.addEventListener('click', stopFunc);
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,88 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const myDateInput = document.getElementById('datetime-picker');
+Notiflix.Notify.init({
+  position: 'center-top',
+});
 
-flatpickr('#datetime-picker', {});
-console.log(myDateInput);
+const convertedDateInput = document.getElementById('datetime-picker');
+const btnStart = document.querySelector('button[data-start]');
+const html = {
+  days: document.querySelector('span[data-days]'),
+  hours: document.querySelector('span[data-hours]'),
+  minutes: document.querySelector('span[data-minutes]'),
+  seconds: document.querySelector('span[data-seconds]'),
+};
+let selectedDate = null;
+let formattedDate = null;
+let timerId = null;
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function dateConverter(date) {
+  for (const key in date) {
+    date[key] = addLeadingZero(date[key]);
+  }
+  return date;
+}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    console.log(selectedDate);
+    if (selectedDate <= options.defaultDate) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      console.log(options.defaultDate);
+    } else {
+      btnStart.removeAttribute('disabled');
+    }
+  },
+};
+function stopTimer() {
+  clearInterval(timerId);
+  Notiflix.Notify.success('Wake Up!');
+}
+function updateTimer() {
+  const currentDate = new Date();
+  const interval = selectedDate.getTime() - currentDate.getTime();
+  if (interval < 0) {
+    stopTimer();
+    return;
+  }
+  const convertedDate = convertMs(interval);
+  formattedDate = dateConverter(convertedDate);
+  html.days.innerText = formattedDate.days;
+  html.hours.innerText = formattedDate.hours;
+  html.minutes.innerText = formattedDate.minutes;
+  html.seconds.innerText = formattedDate.seconds;
+}
+
+flatpickr('#datetime-picker', options);
+
+btnStart.addEventListener('click', () => {
+  btnStart.setAttribute('disabled', 'disabled');
+  timerId = setInterval(updateTimer, 1000);
+});
